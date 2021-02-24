7d2eae8e6c36f71ded7126386e58f7ffa6082ce5
rraw <r.ryanadiwicaksana@gmail.com>
2021-02-24 12:40:57 +0700

    Tugas Hari 8 update
    Hasil sudah sesuai denga output yang sesuai, namun mungkin dengan cara, trik, dan logika yang berbeda

--- tukar-besar-kecil.php
+++ tukar-besar-kecil.php
@@ -25,5 +25,5 @@ echo tukar_besar_kecil('I aM aLAY'); // "i Am Alay"
 echo tukar_besar_kecil('My Name is Bond!!'); // "mY nAME IS bOND!!"
 echo tukar_besar_kecil('IT sHOULD bE me'); // "it Should Be ME"
 echo tukar_besar_kecil('001-A-3-5TrdYW'); // "001-a-3-5tRDyw"
-
+echo tukar_besar_kecil('coBa COBa AjA');
 ?>
