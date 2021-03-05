9240ac7631bf2b2f7818a1daf5334779cda48516
rraw <r.ryanadiwicaksana@gmail.com>
2021-03-05 07:51:30 +0700

    Tugas Hari 14

---
+++ blog/database/migrations/2021_03_05_000511_create_pertanyaan_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePertanyaanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pertanyaan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            $table->string('judul');
+            $table->longText('isi');
+            $table->date('tanggal_dibuat');
+            $table->date('tanggal_diperbaharui');
+            $table->foreign('profile_id')->references('id')->on('profiles');
+            $table->foreign('jawaban_tepat_id')->references('id')->on('jawaban');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pertanyaan');
+        
+    }
+}
---
+++ blog/database/migrations/2021_03_05_003409_create_like_dislike_jawaban_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLikeDislikeJawabanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('like_dislike_jawaban', function (Blueprint $table) {
+            $table->timestamps();
+            $table->foreign('profile_id')->references('id')->on('profiles');
+            $table->foreign('jawaban_id')->references('id')->on('jawaban');
+            $table->integer('poin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('like_dislike_jawaban');
+    }
+}
